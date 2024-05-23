82c109d137345f12f5c65324e1a508a4f39e895a
vyzakh <vysakh.mp@zoft.care>
2024-05-23 10:57:38 +0530

    first commit

---
+++ .scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started..."
+
+# Pull the latest version of the app
+git pull origin master
+echo "New changes copied to server !"
+
+echo "Installing Dependencies..."
+npm install --yes
+
+echo "Creating Production Build..."
+# For ReactJS VueJS and Nuxt JS
+npm run build
+
+# For NextJS
+# npm run export
+
+echo "Deployment Finished!"
